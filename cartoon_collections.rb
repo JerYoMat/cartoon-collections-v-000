7b5093b7c9ab6f7f9d9e03c803aaa67dd6ad4532
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 23:19:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(word_array)
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
