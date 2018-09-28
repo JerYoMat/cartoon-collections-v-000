c760099960cc99871ae93f4b2ccab813504ea4cc
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 23:24:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,11 @@ end
 def find_the_cheese(string_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each_with_index do |cheese, i|
-    return cheese if cheese == string_array[i]
-    return nil if cheese != string_array[i]
+  matches = []
+  string_array.select do |string|
+    cheese_types.each do |cheese|
+      matches << cheese if cheese == string 
+    end 
   end 
+  matches[0]
 end
